d35cb760c8685df029874948218e672cdedabc20
janesconference <janesconference@bitterspring.net>
2014-03-09 11:49:50 +0000

    Added note names

--- script.js
+++ script.js
@@ -400,15 +400,42 @@ var PianoView = function (el, minOct, octaves) {
   this.minOct = minOct;
   this.octaves = octaves;
 
+  //var keyboardNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  var keyboardNotes = ["B", "A#", "A", "G#", "G", "F#", "F", "E", "D#", "D", "C#", "C"];
+
   var nNotes = octaves * 12;
+
   for (var i = 0; i < nNotes; i+=1) {
-    this.ctx.fillStyle = '#ffffff';
-    this.ctx.strokeStyle = '#230000';
+
+    var noteName = keyboardNotes[i % 12];
+    var oct = minOct + Math.floor (i/12);
+
+    var accident = (noteName.charAt(1) === "#");
+
+    if (accident) {
+      this.ctx.fillStyle = "black";
+    }
+    else {
+      this.ctx.fillStyle = "white";
+    }
+
     var width = this.tw;
     var height = this.th / nNotes;
     var top = i * height;
     var left = 0;
     this.ctx.fillRect(left, top, width, height - 1);
+
+    if (accident) {
+      this.ctx.fillStyle = "white";
+    }
+    else {
+      this.ctx.fillStyle = "black";
+    }
+    
+    var noteString = noteName + " " + oct;
+    console.log (noteString);
+    this.ctx.font = "bold 12px sans-serif";
+    this.ctx.fillText(noteString, 20, top + 18);
   }
 
 }
