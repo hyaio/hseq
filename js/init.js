97896d478e8fd267dc1f817f6cacc120e33ee4df
janesconference <janesconference@bitterspring.net>
2014-03-29 22:35:12 +0000

    Transformed all the ids in classes

--- js/init.js
+++ js/init.js
@@ -24,7 +24,7 @@ var pluginFunction = function () {
     this.backToSeqButton = document.querySelector(".back-to-seq");
     this.patternMainLabel = document.querySelector(".pattern-main-label");
     this.resetButton = document.querySelector(".reset-button");
-    this.controlSelector = document.querySelector("#control-selector");
+    this.controlSelector = document.querySelector(".control-selector");
 
     this.backToSeqButton.addEventListener("click", function () {
         this.patternSequencerDiv.classList.remove("hidden");
@@ -65,13 +65,13 @@ var pluginFunction = function () {
     });
 
 // INIT PATTERN EDITOR
-    this.sheet = document.querySelector("#sheet");
+    this.sheet = document.querySelector(".sheet");
     this.snapMenu = document.querySelector(".snap");
     this.durationMenu = document.querySelector(".newnote");
-    this.piano = document.querySelector("#piano");
-    this.controls = document.querySelector("#controls");
+    this.piano = document.querySelector(".piano");
+    this.controls = document.querySelector(".controls");
 
-    this.rollView = new RollView(sheet);
+    this.rollView = new RollView(this.sheet);
     this.snapMenu.addEventListener("change", function (e) {
         this.rollView.setStep(parseFloat(e.target.value, 10));
         this.rollView.render();
@@ -80,9 +80,9 @@ var pluginFunction = function () {
         this.rollView.setDefaultDuration(parseFloat(e.target.value, 10));
     }.bind(this));
 
-    this.pianoView = new PianoView(piano, 2, 5);
+    this.pianoView = new PianoView(this.piano, 2, 5);
 
-    this.controlView = new ControlView(controls);
+    this.controlView = new ControlView(this.controls);
 };
 
 var plugin = new pluginFunction();
